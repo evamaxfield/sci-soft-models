36505d035ab5800edbccfd5e60240560d0fd2a90
evamaxfield <evamaxfieldbrown@gmail.com>
2024-10-08 13:39:01 -0700

    Add model details object to module

---
+++ sci_soft_models/types.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from dataclasses import dataclass
+
+from dataclasses_json import DataClassJsonMixin
+
+###############################################################################
+
+@dataclass
+class ModelDetails(DataClassJsonMixin):
+    name: str
+    version: str
